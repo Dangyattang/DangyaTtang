2f09ee012e9e23318213c5740b8ba7728aff7781
ChangHyun Park <buschanny@gmail.com>
2025-03-11 17:10:49 +0900

    init setting

---
+++ app.py
@@ -0,0 +1,51 @@
+from bson import ObjectId
+from pymongo import MongoClient
+
+from flask import Flask, render_template, jsonify, request
+from flask.json.provider import JSONProvider
+
+import json
+
+app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)
+db = client.dbjungle
+
+
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+
+class CustomJSONProvider(JSONProvider):
+    def dumps(self, obj, **kwargs):
+        return json.dumps(obj, **kwargs, cls=CustomJSONEncoder)
+
+    def loads(self, s, **kwargs):
+        return json.loads(s, **kwargs)
+
+
+app.json = CustomJSONProvider(app)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+#로그인
+
+#회원가입
+
+#팀 주문 등록
+
+#팀 주문 전체 조회
+
+#팀 주문 선택조회
+
+#
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
+
